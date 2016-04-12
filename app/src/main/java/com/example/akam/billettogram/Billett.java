c8fa09b65427a038df2d71230c8d991fea36ec52
Akam Neeson <akam_p@hotmail.com>
2016-04-12 14:16:05 +0200

    tirsdag 12-04-16

--- app/src/main/java/com/example/akam/billettogram/Billett.java
+++ app/src/main/java/com/example/akam/billettogram/Billett.java
@@ -5,18 +5,27 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 public class Billett extends AppCompatActivity {
 
+    DBAdapter db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_billett);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        db = new DBAdapter(this);
+        db.open();
+
+        Bundle extras = getIntent().getExtras();
+        Log.d("TAG:", "" + extras.getInt("TryThis"));
+
+
 
     }
 
